7174d2e9076f03410d9e5ec38400f0d8c4516927
Felipe Martinez <femrtnz@gmail.com>
2016-09-19 10:35:04 -0300

    Cleaning json class

--- src/main/java/com/wlu/orm/hbase/schema/value/JsonNodeValue.java
+++ src/main/java/com/wlu/orm/hbase/schema/value/JsonNodeValue.java
@@ -28,44 +28,17 @@ public class JsonNodeValue implements Value {
 
 	@Override
 	public byte[] toBytes() {
-//		StringBuilder sb = new StringBuilder();
-//		ObjectMapper om = new ObjectMapper();
 		String result = "";
 		try {
 			result = new ObjectMapper().writeValueAsString(jsonNode);
 		} catch (IOException e) {
 			throw new RuntimeException("Error trying to parse json");
 		}
-//		jsonNode.getElements().forEachRemaining(a -> {
-//			sb.append(jsonNode.asText());
-//		});
 		return Bytes.toBytes(result);
 	}
 
 	@Override
 	public String getType() {
 		return "JsonNodeValue";
-	}
-
-	
-	public static void main(String[] args) throws JsonProcessingException, IOException {
-		List<String> asList = Arrays.asList("Android","IOS","");
-		ObjectMapper mapper = new ObjectMapper();// system:android:AAPPAA1
-		JsonNode jsonnode = mapper.valueToTree(asList);
-//		final String json = "[\"One\", \"Two\", \"Three\"]";
-		 
-		String readValue = new ObjectMapper().writeValueAsString(jsonnode);
-		
-		System.out.println(readValue);
-		
-//		List<String> items = Arrays.asList(readValue);
-		JsonNode jsonnodae = mapper.valueToTree(readValue);
-		System.out.println(jsonnodae.asText());
-//		if (arrNode.isArray()) {
-//		    for (final JsonNode objNode : arrNode) {
-//		        System.out.println(objNode);
-//		    } 
-//		} 
-		
 	}
 }
